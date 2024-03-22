33629041fda08c408adb7edff6cf8382155674dd
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2024-03-22 17:30:06 -0300

    feat[exercises]: exercise 1 completed

---
+++ 1/exercise1.js
@@ -0,0 +1,12 @@
+function startWhile() {
+    const INDICE = 13;
+    let SOMA = 0;
+    let K = 0;
+
+    while (K < INDICE) {
+        K++;
+        SOMA = SOMA + K;
+    }
+
+    console.log('Soma: ', SOMA);
+}
---
+++ 2/exercise2.js
@@ -0,0 +1,30 @@
+function startFibonacci() {
+    let valor = 0;
+    let valor2 = 1;
+    let nextValue = 0;
+    let valueEntered = 60;
+    
+    console.log('valor digitado: ', valueEntered);
+
+    console.log(valor);
+
+    nextValue = valor + nextValue;
+
+    
+   
+    if(valueEntered > 70) {
+        alert('[Erro]: digite um valor menor do que 70');
+    } else {
+        while (nextValue <= valueEntered) {
+            valor = valor2;
+            valor2 = nextValue;
+            nextValue = valor + valor2;
+
+            if(nextValue > 70) {
+                console.log('Acabou!');
+            } else {
+                console.log(nextValue);
+            }            
+        }
+    }    
+}
